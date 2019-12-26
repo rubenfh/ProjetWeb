f2f8cfa969336cfa8051f3082a23fed437e628ab
ClemenF <clemen.fayol@gmail.com>
2019-12-26 17:24:37 +0100

    step 2

--- src/secur.ts
+++ src/secur.ts
@@ -8,12 +8,14 @@ function encrypt(text:any) {
     let cipher = cryp.createCipheriv('aes-256-cbc', Buffer.from(key), iv);
     let encrypted = cipher.update(text);
     encrypted = Buffer.concat([encrypted, cipher.final()]);
-    return { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') };
+    return { iv: iv.toString('hex'), key: key.toString('hex'),encryptedData: encrypted.toString('hex') };
 }
 
-function decrypt(iv:any,encryptedText:any) {
+function decrypt(iv:any,encryptedText:any,key:any) {
     let decipher = cryp.createDecipheriv('aes-256-cbc', Buffer.from(key), iv);
+    console.log("iv ok :"+decipher);    
     let decrypted = decipher.update(encryptedText);
+    console.log("decrypted ok :"+decrypted);
     decrypted = Buffer.concat([decrypted, decipher.final()]);
     return decrypted.toString();
 }
