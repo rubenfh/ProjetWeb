6ccfd7c9169d23073f625cb837dfd88acf7ebe8f
ClemenF <36438932+ClemenF@users.noreply.github.com>
2019-12-27 16:38:37 +0100

    Update handles.ts
    conflit hello getter

--- src/handles.ts
+++ src/handles.ts
@@ -58,12 +58,12 @@ Router.get("/signUp", (req: any, res: any) => {
     .render("signUp.ejs")
 });
 
-Router.get("/hello", (req: any, res: any) => {
-  res
-    .type("html")
-    .status(200)
-    .render("hello.ejs")
-});
+// Router.get("/hello", (req: any, res: any) => {
+//  res
+//    .type("html")
+//    .status(200)
+//    .render("hello.ejs")
+//});
 
 Router.get('/read', (req:any, res:any) => {
   User.find({}).then((result: any) => {
@@ -86,8 +86,8 @@ Router.get('/write', (req:any, res:any) => {
   });
 });
 
-Router.get("/hello:id", async (request: any, response: any, next: any) => {
-  var userId = request.params.id
+//Router.get("/hello:id", async (request: any, response: any, next: any) => {
+//  var userId = request.params.id
   /*try {
     if (request.query.search) {
       const searchquery = request.query.search;
@@ -102,12 +102,12 @@ Router.get("/hello:id", async (request: any, response: any, next: any) => {
   } catch(err){
 throw new Error(err);
   }*/
-  User.find({id: userId}).then((result: any) => {
-    response.status(200).json(result);
-  }).catch((err: any) => {
-    response.status(400).send(err.message);
-  });
-  });
+//  User.find({id: userId}).then((result: any) => {
+//    response.status(200).json(result);
+//  }).catch((err: any) => {
+//    response.status(400).send(err.message);
+//  });
+//  });
 
 
 
