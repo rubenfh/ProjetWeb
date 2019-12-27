50c6f929bc4077d3435211de103b91fe63e4e2fb
ClemenF <clemen.fayol@gmail.com>
2019-12-27 19:34:19 +0100

    updating metrics

--- src/user.ts
+++ src/user.ts
@@ -24,24 +24,6 @@ const userSchema = new mongoose.Schema({
     }
 });
 
-//  userSchema.pre('save',function(this: any, next:any){
-//     this.password= encrypt(this.password)
-//     next();
-//  });
 
-//  userSchema.methods.comparePassword= function(candidatePassword:any){
-//      return new Promise((resolve:any,reject:any)=>{
-//          this.password=decrypt(this.password)
-//          if(this.password!=candidatePassword){
-//              reject(false);
-//          }
-//          else{
-            
-//              resolve(true);
-//          }
-//      }
-//      )
-//  }
-
-const User = mongoose.model('User', userSchema);
+var User = mongoose.model('User', userSchema);
 export = User;
